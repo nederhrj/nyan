72871a2c3a774587ce757c6b6f57e3161016a49b
Rene Nederhand <rene@nederhand.net>
2013-10-17 14:57:01 +0200

    add vendors to mongodb

---
+++ nyan/shared_modules/unit_tests/create_vendor_table_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import logging
+from mongoengine import *
+from nyan.shared_modules.models.mongodb_models import *
+
+""" Creates vendors in mongodb """
+
+
+def add_vendors():
+    for vendor in ["techcrunch", "allthingsd", "allfacebook", "androidandme", "anandtech", "boingboing", "bgr", "engadget"]:
+        logging.info("add vendor" + vendor)
+        vendor = Vendor(name=vendor, config="vendor config " + vendor)
+        vendor.save()
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='test.log',level=logging.DEBUG)
+
+    try:
+        connect("nyan_test")
+    except Exception as e:
+        logging.error("Could not connect to MongoDB due to error %s: %s" % (type(e), e))
+
+    try:
+        add_vendors()
+    except Exception as e:
+        logging.error("Could add vendor due to error %s: %s" % (type(e), e))
+
